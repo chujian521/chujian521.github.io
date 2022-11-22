e87ff70c7fbc490b5edf61ba67e16821a1f1ed08
Xin Li <1583810565@qq.com>
2022-11-22 22:23:14 +0800

    Site updated: 2022-11-22 22:23:04

---
+++ js/funnyTitle.js
@@ -0,0 +1,15 @@
+<!--浏览器整蛊标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         clearTimeout(titleTime);
+     }
+     else {
+         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
